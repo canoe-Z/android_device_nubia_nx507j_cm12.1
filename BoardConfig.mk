52482dbc9fc84d489ab09ed556a37821312e9d36
liwentao0705 <liwentao0705@outlook.com>
2015-08-21 15:36:02 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -216,9 +216,11 @@ PRODUCT_COPY_FILES := $(filter-out frameworks/base/data/keyboards/AVRCP.kl:syste
 # Packages
 PRODUCT_PACKAGES := $(filter-out BluetoothExt Development Profiles VoicePlus Basic Launcher3 AudioFX CMWallpapers CMFileManager CMUpdater CMAccount CMHome Galaxy4 HoloSpiralWallpaper LiveWallpapers MagicSmokeWallpapers NoiseField PhaseBeam VisualizationWallpapers PhotoTable SoundRecorder PhotoPhase LatinIME Stk CellBroadcastReceiver WhisperPush Terminal BasicDreams Calendar CalendarProvider CaptivePortalLogin Email Exchange2 ExternalStorageProvider InputDevices Launcher2 PicoTts PacProcessor PrintSpooler ProxyHandler QuickSearchBox SharedStorageBackup BackupRestoreConfirmation HTMLViewer Shell WAPPushManager Music MusicFX OneTimeInitializer nano, $(PRODUCT_PACKAGES))
 
-#RazerRom
+BLISS_BUILD_BLOCK := 1
+BLISS_WIPE_CACHES := 0
 TARGET_TC_ROM := 5.1-sm
 TARGET_TC_KERNEL := 5.1-sm
+BLISSIFY := true
 BLISS_O3 := true
 BLISS_GRAPHITE := true
 BLISS_STRICT := true
@@ -227,6 +229,7 @@ BLISS_PIPE := true
 
 TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
 
 # SaberMod
 -include vendor/razer/config/sm.mk
