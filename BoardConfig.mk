c3acf942e638797e3113eed77926c55b57179c00
liwentao0705 <liwentao0705@outlook.com>
2015-08-24 16:48:51 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES := $(filter-out BluetoothExt Development Profiles VoicePlus Bas
 # BlissPop Config Flags
 BLISS_WIPE_CACHES := 0
 TARGET_TC_ROM := 5.1-sm
-TARGET_TC_KERNEL := 4.9-hybrid
+TARGET_TC_KERNEL := 4.9-linaro
 BLISSIFY := true
 BLISS_O3 := true
 BLISS_STRICT := true
